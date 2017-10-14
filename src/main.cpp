f421721ef1bdeee216ec59269e92af2870a2c2b7
yossico <yossicohen19@gmail.com>
2017-10-14 16:48:49 +0300

    smoother path

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,10 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
+using namespace tk;
 
 // for convenience
 using json = nlohmann::json;
@@ -18,6 +20,7 @@ using json = nlohmann::json;
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+const double LANEWIDTH 4;
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -162,6 +165,9 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 int main() {
   uWS::Hub h;
 
+  double lane = 1.0; //points to teh current lane, 1 is the middle
+  double ref_velocity = 49;
+
   // Load up map values for waypoint's x,y,s and d normalized normal vectors
   vector<double> map_waypoints_x;
   vector<double> map_waypoints_y;
@@ -229,14 +235,45 @@ int main() {
           	// Previous path's end s and d values 
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
-
-          	// Sensor Fusion Data, a list of all other cars on the same side of the road.
+			// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
-          	json msgJson;
+			int prev_size = previous_path_x.size();
 
-			//create waypoints vector. must fill this to make the car move (walkthrough)
-          	vector<double> next_x_vals;
+			vector<double> ptsx;
+			vector<double> ptsy;
+			double ref_x = car_x;
+			double ref_y = car_y;
+			double ref_yow = deg2rad(car_yow);
+
+			if (prev_size < 2)  /in case we just started no ref points
+			{
+				double prev_car_x = car_x - cos(car_yow) ;
+				double prev_car_y = car_y - sin(car_yaw);
+				
+				ptsx.push_back(prev_car_x);
+				ptsx.push_back(car_x);
+
+				ptsy.push_back(prev_car_y);
+				ptsy.push_back(car_y);
+
+			}
+			else
+			{
+				ref_x = previous_path_x[prev_size-1];
+				double ref_x_prev = previous_path_x[prev_size - 2];
+
+				ref_y = previous_path_y[prev_size - 1];
+				double ref_y_prev = previous_path_y[prev_size - 2];
+
+				//push back previous path points
+				ptsx.push_back(ref_x_prev);
+				ptsx.push_back(ref_x);
+				ptsy.push_back(ref_y_prev);
+				ptsy.push_back(ref_y);
+			}
+			//initial walkthrough code
+          	/*vector<double> next_x_vals;
           	vector<double> next_y_vals;
 			double dist_inc = 0.3;
 			for (int i = 0; i < 50; i++)
@@ -247,9 +284,73 @@ int main() {
 				
 				next_x_vals.push_back(xy[0]);
 				next_y_vals.push_back(xy[1]);
+			}*/
+			//create waypoints vector. must fill this to make the car move (walkthrough)
+			vector <double> next_wp0 = getXY(car_s + 30, LANEWIDTH*(lane + .5), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> next_wp1 = getXY(car_s + 60, LANEWIDTH*(lane + .5), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> next_wp2 = getXY(car_s + 90, LANEWIDTH*(lane + .5), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			ptsx.push_back(next_wp0[0]);
+			ptsx.push_back(next_wp1[0]);
+			ptsx.push_back(next_wp2[0]);
+
+			ptsy.push_back(next_wp0[1]);
+			ptsy.push_back(next_wp1[1]);
+			ptsy.push_back(next_wp2[1]);
+
+			
+			for (int i = 0; i < ptsx.size; i++)
+			{
+				double shift_x = ptsx[i] - ref_x;
+				double shift_y = ptsx[i] - ref_x;
+
+				ptsx[i] = shift_x*cos(-ref_yaw) - shift_y*sin(-ref_yaw);
+				ptsy[i] = shift_x*sin(-ref_yaw) + shift_y*cos(-ref_yaw);
 			}
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+			spline s;
+			s.set_points(ptsx, ptsy);
+
+			vector<double> next_x_vals;
+			vector<double> next_y_vals;
+
+			//add the already calculated previous points to the path
+			for (int i = 0; i < previous_path_x.size(); i++)
+			{
+				next_x_vals.push_back(previous_path_x[i]);
+				next_y_vals.push_back(previous_path_y[i]);
+			}
+			double target_x = 30.0;   //the distance to the first point is 30
+			double target_y = s(target_x); // we calculate the y value for that x with the spline
+ 		    
+			// the distance is the hypotenuse of the triangle made with X & Y
+			double target_dist = sqrt((target_x*target_x) + (target_y*target_x)); 
+			double x_add_on = 0;
+
+			for (int i = 1; i < 50-previous_path_x.size(); i++)
+			{
+				double N = target_dist/(.02*ref_velocity/ 2.24);
+				double x_point = x_add_on + target_x / N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+
+				double x_ref = x_point;
+				double y_ref = y_point;
+				// rotate back ccordinates
+				x_point = x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw);
+				y_point = x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw);
+				x_point += ref_x;
+				y_point += ref_y;
+
+				// add points to vector
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+
+			}
+
+			//create the control/utput msg 
+			json msgJson;
+			// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
