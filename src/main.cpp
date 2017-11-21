e73163f5a33662714ce14c097ab17959d3de7169
yossico <yossicohen19@gmail.com>
2017-11-21 17:57:18 +0200

    simple works

--- src/main.cpp
+++ src/main.cpp
@@ -249,7 +249,7 @@ int main() {
 			for (int i = 0; i < sensor_fusion.size(); i++)
 			{
 				float d = sensor_fusion[i][6];
-				if ((d < 2 + 4 * lane+2) && (d > 2 + 4 * lane - 2))
+				if ((d < 2 + LANEWIDTH * lane+2) && (d > 2 + LANEWIDTH * lane - 2))
 				{
 					double vx = sensor_fusion[i][3];
 					double vy = sensor_fusion[i][4];
@@ -276,7 +276,7 @@ int main() {
 			{
 				ref_velocity -= 0.224;
 			}
-			else if (ref_velocity < 49)
+			else if (ref_velocity < 48.5)
 			{
 				ref_velocity += 0.224;
 			}
@@ -315,23 +315,11 @@ int main() {
 				ptsy.push_back(ref_y_prev);
 				ptsy.push_back(ref_y);
 			}
-			//initial walkthrough code
-          	/*vector<double> next_x_vals;
-          	vector<double> next_y_vals;
-			double dist_inc = 0.3;
-			for (int i = 0; i < 50; i++)
-			{
-				double next_s = car_s + (i + 1)*dist_inc;
-				double next_d = 6;
-				vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-				
-				next_x_vals.push_back(xy[0]);
-				next_y_vals.push_back(xy[1]);
-			}*/
+
 			//create waypoints vector. must fill this to make the car move (walkthrough)
-			vector <double> next_wp0 = getXY(car_s + 30, LANEWIDTH*lane + 2, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-			vector <double> next_wp1 = getXY(car_s + 60, LANEWIDTH*lane + 2, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-			vector <double> next_wp2 = getXY(car_s + 90, LANEWIDTH*lane + 2, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> next_wp0 = getXY(car_s + 30, LANEWIDTH*(lane + 0.5), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> next_wp1 = getXY(car_s + 60, LANEWIDTH*(lane + 0.5), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector <double> next_wp2 = getXY(car_s + 90, LANEWIDTH*(lane + 0.5), map_waypoints_s, map_waypoints_x, map_waypoints_y);
 			ptsx.push_back(next_wp0[0]);
 			ptsx.push_back(next_wp1[0]);
 			ptsx.push_back(next_wp2[0]);
@@ -444,3 +432,18 @@ int main() {
   }
   h.run();
 }
+
+
+//initial walkthrough code
+/*vector<double> next_x_vals;
+vector<double> next_y_vals;
+double dist_inc = 0.3;
+for (int i = 0; i < 50; i++)
+{
+double next_s = car_s + (i + 1)*dist_inc;
+double next_d = 6;
+vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+next_x_vals.push_back(xy[0]);
+next_y_vals.push_back(xy[1]);
+}*/
