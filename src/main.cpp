5d6b872763ba5350015db810029ca3bc09b6c56c
yossico <yossicohen19@gmail.com>
2017-12-07 08:02:27 +0200

    aa

--- src/main.cpp
+++ src/main.cpp
@@ -139,7 +139,7 @@ vector<double> getFrenet(double x, double y, double theta, const vector<double> 
 }
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &Pointss_s, const vector<double> &maps_x, const vector<double> &maps_y)
+/*vector<double> getXY(double s, double d, const vector<double> &Pointss_s, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 	int prev_wp = -1;
 
@@ -164,9 +164,36 @@ vector<double> getXY(double s, double d, const vector<double> &Pointss_s, const 
 
 	return {x,y};
 
-}
+}*/
 double lane = 1.0;
 
+// Transform from Frenet s,d coordinates to Cartesian x,y
+vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
+{
+	int prev_wp = -1;
+
+	while (s > maps_s[prev_wp + 1] && (prev_wp < (int)(maps_s.size() - 1)))
+	{
+		prev_wp++;
+	}
+
+	int wp2 = (prev_wp + 1) % maps_x.size();
+
+	double heading = atan2((maps_y[wp2] - maps_y[prev_wp]), (maps_x[wp2] - maps_x[prev_wp]));
+	// the x,y,s along the segment
+	double seg_s = (s - maps_s[prev_wp]);
+
+	double seg_x = maps_x[prev_wp] + seg_s*cos(heading);
+	double seg_y = maps_y[prev_wp] + seg_s*sin(heading);
+
+	double perp_heading = heading - pi() / 2;
+
+	double x = seg_x + d*cos(perp_heading);
+	double y = seg_y + d*sin(perp_heading);
+
+	return { x,y };
+
+}
 
 
 
