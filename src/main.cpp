012c03910eff61dca0481638a1f834195fae69b8
yossico <yossicohen19@gmail.com>
2017-12-10 11:28:13 +0200

    add Points

--- src/main.cpp
+++ src/main.cpp
@@ -241,7 +241,7 @@ int main() {
 		map_waypoints_dy.push_back(d_y);
 	}
 */
-	h.onMessage([&myroad, &myPlanner, myPoints /*&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy*/](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+	h.onMessage([&myroad, &myPlanner, &myPoints /*&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy*/](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
 		uWS::OpCode opCode) {
 		// "42" at the start of the message means there's a websocket message event.
 		// The 4 signifies a websocket message
