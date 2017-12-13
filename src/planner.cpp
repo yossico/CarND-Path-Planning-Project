a948ba40444155757008b15de4ae6aa8c9f68bd9
yossico <yossicohen19@gmail.com>
2017-12-13 09:45:08 +0200

    fixing

--- src/planner.cpp
+++ src/planner.cpp
@@ -277,7 +277,7 @@ void Planner::GetJMTPathPoints(Points& points, vector<vector<double>>& trajector
 void Planner::apply_action(Vehicle& car, LANE current_lane, LANE target_lane) {
 	car.set_previous_s(this->end_s);
 	car.set_previous_d(this->end_d);
-	set_state(current_lane, target_lane);
+	this->set_state(current_lane, target_lane);
 }
 
 /* ACTIONS */
@@ -285,7 +285,7 @@ void Planner::start_car(Vehicle& car) {
 	cout << "ACTION: start_car" << endl;
 	this->n = 4 * POINTS; // 4 cycles to start
 	double target_v = SPEED_LIMIT*0.5;
-	double target_s = car.get_s() + n * AT * target_v;
+	double target_s = car.get_s() + n * AT * target_v;;
 
 	this->start_s = { car.get_s(), car.get_v(), 0.0 };
 	this->end_s = { target_s, target_v, 0.0 };
@@ -317,35 +317,34 @@ void Planner::reduce_speed(Vehicle& car) {
 	cout << "ACTION: reduce_speed" << endl;
 	this->n = CYCLES*POINTS;
 	this->new_points = true;
-	double target_v = max(car.get_v()*0.8, SPEED_LIMIT / 2);
-	double target_s = car.get_s() + n * AT * target_v;
+	double target_v = max(car.prev_s()[1] * 0.8, SPEED_LIMIT / 2);
+	double target_s = car.prev_s()[0] + n * AT * target_v;
 
-	this->start_s = { car.get_s(), car.get_v(), car.prev_s()[2] };
+	this->start_s = { car.prev_s()[0], car.prev_s()[1], car.prev_s()[2] };
 	this->end_s = { target_s, target_v, 0.0 };
 
-	double target_d = get_lane_d(car.get_d());
+	double target_d = get_lane_d(car.prev_d()[0]);
 
-	this->start_d = { car.get_d(), 0.0, 0.0 };
+	this->start_d = { get_lane_d(car.prev_d()[0]), 0.0, 0.0 };
 	this->end_d = { target_d, 0.0, 0.0 };
 
-	this->apply_action(car, get_lane(car.get_d()), get_lane(target_d));
+	this->apply_action(car, get_lane(car.prev_d()[0]), get_lane(car.prev_d()[0]));
 }
 
 void Planner::change_lane(Vehicle& car, LANE target_lane) {
 	cout << "ACTION: reduce_speed" << endl;
 	this->n = CYCLES*POINTS;
 	this->new_points = true;
-	double target_v = car.get_v();
-	double target_s = car.get_s() + n * AT * target_v;
+	double target_v = car.prev_s()[1];
+	double target_s = car.prev_s()[0] + n * AT * target_v;
 
-	this->start_s = { car.get_s(), car.get_v(), car.prev_s()[2] };
+	this->start_s = { car.prev_s()[0], car.prev_s()[1], car.prev_s()[2] };
 	this->end_s = { target_s, target_v, 0.0 };
 
 	double target_d = get_lane_d(target_lane);
 
-	this->start_d = { car.get_d(), 0.0, 0.0 };
+	this->start_d = { get_lane_d(car.prev_d()[0]), 0.0, 0.0 };
 	this->end_d = { target_d, 0.0, 0.0 };
 
-	this->apply_action(car, get_lane(car.get_s()), get_lane(target_d));
+	this->apply_action(car, get_lane(car.prev_d()[0]), get_lane(target_d));
 }
-
